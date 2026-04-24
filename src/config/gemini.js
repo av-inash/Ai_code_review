5056332505dc1aaa754ec2c3cb1d27dca4923244
av-inash <hydroavinash@gmail.com>
2026-04-24 17:05:11 +0530

    put some console

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const apiKey = process.env.GEMINI_API_KEY;
+const prot=process.env.PORT  
+console.log("prottttt",prot)
+console.log("gemni keyyyy",apiKey)
 if (!apiKey) {
   throw new Error("GEMINI_API_KEY is missing in .env file");
 }
